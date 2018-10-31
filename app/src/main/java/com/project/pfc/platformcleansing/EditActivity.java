3e5e0808907860d2fe225e98759132dc6226f1a6
인모 킴 <ko3036@gamil.com>
2018-10-31 18:19:41 +0900

    edit actionbar
    에딧 액션바

--- app/src/main/java/com/project/pfc/platformcleansing/EditActivity.java
+++ app/src/main/java/com/project/pfc/platformcleansing/EditActivity.java
@@ -1,9 +1,29 @@
 package com.project.pfc.platformcleansing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class EditActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.edit_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()) {
+            case R.id.edit_save:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

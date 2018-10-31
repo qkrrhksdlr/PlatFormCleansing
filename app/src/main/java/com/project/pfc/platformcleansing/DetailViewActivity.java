36ce1dcc1bf7365c4e46cc1bd7cdcf93e2c3fc49
parkgwanik <antonio2929@naver.com>
2018-10-31 18:04:29 +0900

    디테일뷰 액션바 추가

--- app/src/main/java/com/project/pfc/platformcleansing/DetailViewActivity.java
+++ app/src/main/java/com/project/pfc/platformcleansing/DetailViewActivity.java
@@ -1,7 +1,11 @@
 package com.project.pfc.platformcleansing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class DetailViewActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class DetailViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.detail_menu, menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.edit_activity:
+                startActivity(new Intent(getApplicationContext(), EditActivity.class));
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
     }
 }

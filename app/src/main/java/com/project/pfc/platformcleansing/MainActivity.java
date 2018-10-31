513b30c2231de2c4fcaf5a55b690d039127795d3
parkgwanik <antonio2929@naver.com>
2018-10-31 15:11:21 +0900

    리스너 추가

--- app/src/main/java/com/project/pfc/platformcleansing/MainActivity.java
+++ app/src/main/java/com/project/pfc/platformcleansing/MainActivity.java
@@ -2,7 +2,10 @@ package com.project.pfc.platformcleansing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,16 +17,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<BunkerItem> data = new ArrayList<BunkerItem>();
-        data.add(new BunkerItem(R.drawable.bunker1,"테란벙커", "마사라"));
-        data.add(new BunkerItem(R.drawable.bunker2,"바다벙커", "해변"));
-        data.add(new BunkerItem(R.drawable.bunker3,"위장벙커", "잔디밭"));
-        data.add(new BunkerItem(R.drawable.bunker4,"민간벙커", "슈퍼앞"));
-        data.add(new BunkerItem(R.drawable.bunker5,"Shelter", "에란겔"));
-        data.add(new BunkerItem(R.drawable.bunker6,"지하벙커", "숲"));
+        data.add(new BunkerItem(R.drawable.bunker1, "테란벙커", "마사라"));
+        data.add(new BunkerItem(R.drawable.bunker2, "바다벙커", "해변"));
+        data.add(new BunkerItem(R.drawable.bunker3, "위장벙커", "잔디밭"));
+        data.add(new BunkerItem(R.drawable.bunker4, "민간벙커", "슈퍼앞"));
+        data.add(new BunkerItem(R.drawable.bunker5, "Shelter", "에란겔"));
+        data.add(new BunkerItem(R.drawable.bunker6, "지하벙커", "숲"));
 
         bunkerAdapter = new CustomAdapter(this, R.layout.item, data);
 
         ListView bunkerList = findViewById(R.id.bunker_list);  //메인 리스트뷰
         bunkerList.setAdapter(bunkerAdapter);
+
+        bunkerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), DetailViewActivity.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, R.string.msg, Toast.LENGTH_SHORT).show(); // 테스트용 토스트
+            }
+        });
     }
 }

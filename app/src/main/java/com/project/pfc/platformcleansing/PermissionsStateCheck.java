e93dad97abdab1d057b40975e54f526c7c503b8e
임철순 <skycs97@naver.com>
2018-12-05 21:20:28 +0900

    permissions 체크클래스

---
+++ app/src/main/java/com/project/pfc/platformcleansing/PermissionsStateCheck.java
@@ -0,0 +1,28 @@
+package com.project.pfc.platformcleansing;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+public class PermissionsStateCheck {            // 권한 확인
+    public static final String [] permissions= {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    public static final int permission_location = 0;
+    public static final int permission_read_external = 1;
+    public static final int permission_write_external = 2;
+
+    public boolean permissionState(Context context, int permission_index){                  //받아온 권한 상태 반환
+        return PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(context,permissions[permission_index]);
+    }
+
+    public void setPermissions(Activity activity, int permisson_index, int requestCode){   //받아온 권한 승인 요청
+        ActivityCompat.requestPermissions(activity, new String[] {permissions[permisson_index]}, requestCode);
+    }
+
+    public void setAllPermissions(Activity activity, int requestCode){
+        ActivityCompat.requestPermissions(activity, permissions, requestCode);
+    }
+
+}

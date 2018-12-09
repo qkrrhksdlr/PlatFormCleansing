cf9a964ddbc8f50e9b4dd36ba21e4a46dca9055b
inmok <ko3036@gmail.com>
2018-12-09 21:46:12 +0900

    로그인 중복은 아직
    로그인 중복은 아직

--- app/src/main/java/com/project/pfc/platformcleansing/LoginActivity.java
+++ app/src/main/java/com/project/pfc/platformcleansing/LoginActivity.java
@@ -6,6 +6,7 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,22 +15,24 @@ import android.widget.Toast;
 
 import java.io.File;
 
+import static android.os.Build.ID;
+import static android.provider.Telephony.Carriers.PASSWORD;
+
 public class LoginActivity extends AppCompatActivity {
     private BunkerDBHelper dbHelper;
-    SQLiteDatabase userDatabase;
+
+    final TextView loginText = (TextView) findViewById(R.id.LOGIN_text);
+    final Button btn_Signin = (Button) findViewById(R.id.btn_signin);
+    final Button btn_Signup = (Button) findViewById(R.id.btn_signup);
+    final EditText editID = (EditText) findViewById(R.id.user_id);
+    final EditText editPASS = (EditText) findViewById(R.id.user_pwd);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        userDatabase = init_database();
-
-        final TextView loginText = (TextView) findViewById(R.id.LOGIN_text);
-        final Button btn_Signin = (Button) findViewById(R.id.btn_signin);
-        final Button btn_Signup = (Button) findViewById(R.id.btn_signup);
-        final EditText editID = (EditText) findViewById(R.id.user_id);
-        final EditText editPASS = (EditText) findViewById(R.id.user_pwd);
 
         dbHelper = new BunkerDBHelper(this);
 
@@ -56,28 +59,30 @@ public class LoginActivity extends AppCompatActivity {
         btn_Signup.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                //회원가입 누르면 LOGIN 텍스트와 아디,비번창 + 버튼도 가입하기 로 바꿀수있게이 회원가입 형식으로 바뀌게
-                //id : LOGIN_text ,  ,
+                //회원가입 누르면 LOGIN 텍스트와 버튼이 비뀐다
                 loginText.setText("회 원 가 입");
                 btn_Signup.setText("가입하기");
                 btn_Signin.setEnabled(false);
+
+                btn_Signup.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dbHelper.insertUserData(ID, PASSWORD);
+
+                        btn_Signup.setOnClickListener(new View.OnClickListener(){
+                            @Override
+                            public void onClick(View v) {               //다시 버튼 바꾸고 JOIN버튼 비활도 풀어준다
+                                btn_Signup.setText("회원가입");
+                                btn_Signin.setEnabled(true);
+                            }
+                        });
+
+                    }
+                });
+
             }
         });
-    }
 
-    private SQLiteDatabase init_database() {
-
-        SQLiteDatabase db = null;
-        File userfile = new File(getFilesDir(), "UserDatabase");
-        try{
-            db = SQLiteDatabase.openOrCreateDatabase(userfile, null);
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
-        if(db==null){
-            Toast.makeText(this, "데이터 베이스 불러오기 실패", Toast.LENGTH_SHORT).show();
         }
 
-        return db;
-    }
 }
